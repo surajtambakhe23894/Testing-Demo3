fa3b6fe6eeeaf20ae223edc4dea3b4554bd14c77
Suraj <82778309+surajtambakhe23894@users.noreply.github.com>
2021-04-22 01:46:53 +0530

    Update by suraj--

--- Testing GitHub Demo3/src/GitHub3/Demo3.java
+++ Testing GitHub Demo3/src/GitHub3/Demo3.java
@@ -1,6 +1,10 @@
 package GitHub3;
 
 public class Demo3 {
+	public void m3()
+	{
+	System.out.println("m3 method--");
+	}
 	
 public static void main(String[] args) {
 	System.out.println("Hello main method");
